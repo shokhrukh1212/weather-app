743a16aaa2bba3286e737c5f06cab4427cc481cc
shokhrukhKarimov <shohruxkarimov546@gmail.com>
2023-07-06 12:15:24 +0500

    second part, extending application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,17 @@ export class AppComponent implements OnInit {
   city: string = 'Tashkent';
   temp: number = 0;
   cityName: string = '';
+  weatherDescription: string = '';
+  className: string = '';
   weatherData!: WeatherData;
+
+  humidity: number = 0;
+  windSpeed: number = 0;
+  windDegree: number = 0;
+  maxTemp: number = 0;
+  minTemp: number = 0;
+
+  today: Date = new Date();
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
@@ -28,9 +38,25 @@ export class AppComponent implements OnInit {
   private getWeatherData(city: string) {
     this.weatherService.getWeatherData(city).subscribe({
       next: (res) => {
-        this.temp = Math.round(res.main.temp - 273.15);
+        this.temp = Math.ceil(res.main.temp - 273.15);
         this.cityName = res.name;
         this.weatherData = res;
+        this.weatherDescription = res.weather[0].description.toUpperCase();
+        this.humidity = res.main.humidity;
+        this.windSpeed = res.wind.speed;
+        this.windDegree = res.wind.deg;
+        this.maxTemp = Math.ceil(res.main.temp_max - 273.15);
+        this.minTemp = Math.ceil(res.main.temp_min - 273.15);
+
+        // giving a class name depending on a weather
+        if (this.temp >= 0 && this.temp < 10) {
+          this.className = 'container-snowy';
+        } else if (this.temp >= 5 && this.temp <= 20) {
+          this.className = 'container-rainy';
+        } else {
+          this.className = 'container-shiny';
+        }
+
         console.log(res);
       },
       error: (err) => console.log('Error: ', err),
